ceb4e1fc5fe4a82ef1d592d3fff9c47586dde76b
RichTsague <rich-tresor.tsague-zangue@student.junia.com>
2024-11-02 13:06:28 +0100

    azert

--- drive/forms.py
+++ drive/forms.py
@@ -5,4 +5,4 @@ from .models import Document
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ['nom', 'type_fichier']  # Utilisez 'fichier' pour correspondre au champ dans le modèle
+        fields = ['nom', 'type_fichier',]  # Utilisez 'fichier' pour correspondre au champ dans le modèle
